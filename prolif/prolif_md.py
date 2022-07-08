e54b9f1dc70ef64e1e2ef53659a4d39cf9b88a64
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-07-08 15:51:15 +0200

    Add arguments to additionally select atoms to the protein side and to print progress  (prolif_md.py)

--- prolif/prolif_md.py
+++ prolif/prolif_md.py
@@ -21,16 +21,25 @@ def main():
                         help='residue name of a ligand in the input trajectory.')
     parser.add_argument('-s', '--step', metavar='INTEGER', required=False, default=1, type=int,
                         help='step to take every n-th frame.')
+    parser.add_argument('-a', '--append_protein_selection', metavar='STRING', required=False, default=None,
+                        help='the string which will be concatenated to the protein selection atoms. '
+                             'Example: "resname ZN or resname MG".')
     parser.add_argument('-o', '--output', metavar='FILENAME', required=True,
                         help='output text file name')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='print progress.')
 
     args = parser.parse_args()
     u = mda.Universe(args.topology, args.trajectory)
     lig = u.atoms.select_atoms(f'resname {args.ligand}')
-    prot = u.atoms.select_atoms('protein')
+    if args.append_protein_selection is None:
+        protein_selection = 'protein'
+    else:
+        protein_selection = f'protein or {args.append_protein_selection}'
+    prot = u.atoms.select_atoms(protein_selection)
     fp = plf.Fingerprint(['Hydrophobic', 'HBDonor', 'HBAcceptor', 'Anionic', 'Cationic', 'CationPi', 'PiCation',
                           'PiStacking', 'MetalAcceptor'])
-    fp.run(u.trajectory[::args.step], lig, prot, progress=False)
+    fp.run(u.trajectory[::args.step], lig, prot, progress=args.verbose)
     df = fp.to_dataframe()
     df.columns = ['.'.join(item.strip().lower() for item in items[1:]) for items in df.columns]
     df.to_csv(args.output, sep='\t')
