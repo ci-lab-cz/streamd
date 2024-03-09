8302493e0b4e85abec151f69da549a59b3d7ca06
Aleksandra <avnikonenko.volg@gmail.com>
2024-03-09 13:44:06 +0100

    transform nm to A

--- streamd/scripts/xvg2png.py
+++ streamd/scripts/xvg2png.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import argparse
+import logging
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def convertxvg2png(xvg_file):
+def convertxvg2png(xvg_file, transform_nm_to_A=False):
     def check_if_value_found(value):
         if value:
             return value[0]
@@ -16,7 +18,7 @@ def convertxvg2png(xvg_file):
     data_coord = [i for i in data if not (i.startswith('#') or i.startswith('@'))]
     title = check_if_value_found([i for i in data if ('@' in i and 'title' in i)]).replace('@    title ','').replace('"','')
     subtitle = check_if_value_found([i for i in data if ('@' in i and 'subtitle' in i)]).replace('@ subtitle ','').replace('"','')
-    legend = [i for i in data if ('@ s' in i and 'legend' in i)]
+    legend_list = [i.replace('@ s', '') for i in data if ('@ s' in i and 'legend' in i)]
 
     xaxis = [i for i in data if (i.startswith('@') and 'xaxis' in i)]
     yaxis = [i for i in data if (i.startswith('@') and 'yaxis' in i)]
@@ -32,18 +34,40 @@ def convertxvg2png(xvg_file):
     plt.figure(figsize=(15, 12))
     plt.title(title)
     plt.xlabel(xaxis)
-    plt.ylabel(yaxis)
+    if transform_nm_to_A:
+        yaxis_A = yaxis.replace('nm', 'Angstrom')
+        plt.ylabel(yaxis_A)
+    else:
+        plt.ylabel(yaxis)
 
     if coords and len(coords[0]) == 2:
         d = pd.DataFrame(coords, columns=[xaxis, yaxis])
-        plot1 = plt.plot(d[xaxis], d[yaxis], marker='o', linewidth=2, markersize=5)
+        if transform_nm_to_A and 'nm' in yaxis.lower():
+            logging.warning(f'INFO: {xvg_file} nm ({yaxis}) values are converted in Angstrom ({yaxis_A})')
+            plot1 = plt.plot(d[xaxis], d[yaxis].apply(lambda x: round(x*10,3)), marker='o', linewidth=2, markersize=5)
+        else:
+            plot1 = plt.plot(d[xaxis], d[yaxis], marker='o', linewidth=2, markersize=5)
 
-    elif coords and len(coords[0]) > 2 and legend:
-        d = pd.DataFrame(coords, columns=[xaxis]+legend)
-        plt.legend(legend, loc='upper right', bbox_to_anchor=(0, 0), borderaxespad=-1)
-        plot1 = plt.plot(d[xaxis], d[legend], marker='o', linewidth=2, markersize=5)
+    elif coords and len(coords[0]) > 2 and legend_list:
+        d = pd.DataFrame(coords, columns=[xaxis]+legend_list)
+        plt.legend(legend_list, loc='upper right', bbox_to_anchor=(0, 0), borderaxespad=-1)
+        if transform_nm_to_A and 'nm' in yaxis.lower():
+            logging.warning(f'INFO: {xvg_file} nm ({yaxis}) values are converted in Angstrom ({yaxis_A})')
+            plot1 = plt.plot(d[xaxis], d[legend_list].apply(lambda x: round(x*10,3)), marker='o', linewidth=2, markersize=5)
+        else:
+            plot1 = plt.plot(d[xaxis], d[legend_list], marker='o', linewidth=2, markersize=5)
     if plot1:
         plot1 = plot1[0]
-        plt.legend(legend)
+        plt.legend(legend_list)
         plot1 = plot1.figure.suptitle(subtitle)
         plot1.figure.savefig(xvg_file.replace('.xvg','.png'), bbox_inches="tight")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''Returns the formal charge for the molecule using RDKiT''')
+    parser.add_argument('-i', '--input', metavar='FILENAME', required=True,
+                        help='input file with compound. Supported formats: *.csv')
+    parser.add_argument('--convert_to_A', action='store_true', default=False,
+                        help='convert value from nm to A')
+    args = parser.parse_args()
+
+    convertxvg2png(args.input, transform_nm_to_A=args.convert_to_A)
