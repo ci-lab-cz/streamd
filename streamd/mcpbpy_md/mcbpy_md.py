304cccc48010e9d7b3f005ccb2d1aab0ff58f61b
Aleksandra <avnikonenko.volg@gmail.com>
2025-02-07 11:30:21 +0100

    add Position Restraints for mcpbpy pipline

--- streamd/mcpbpy_md/mcbpy_md.py
+++ streamd/mcpbpy_md/mcbpy_md.py
@@ -142,12 +142,12 @@ def main(wdir_var_ligand, protein_name, protein_file, metal_resnames, metal_char
                              bash_log=bash_log, seed=seed, env=env):
         return None
     #add Position restraints
-    # if not os.path.isfile(os.path.join(wdir_md_cur, 'posre.itp')):
-    #     if not mcpbpy_preparation.create_posre(all_resids=list(molids_pairs_dict.values())+list(set(metal_atomid_dict.values())),
-    #                                     wdir=wdir_md_cur,
-    #                                     bash_log=bash_log_curr):
-    #         return None
-    # mcpbpy_preparation.add_restraints_to_topol(topol=os.path.join(wdir_md_cur, 'topol.top'))
+    if not os.path.isfile(os.path.join(wdir_md_cur, 'posre.itp')):
+        if not mcpbpy_preparation.create_posre(all_resids=list(molids_pairs_dict.values())+list(set(metal_atomid_dict.values())),
+                                        wdir=wdir_md_cur,
+                                        bash_log=bash_log_curr):
+            return None
+    mcpbpy_preparation.add_restraints_to_topol(topol=os.path.join(wdir_md_cur, 'topol.top'))
 
     return wdir_md_cur
 
